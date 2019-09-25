58b4955f436de32981c6419f0544cd47b6e89175
Vinicius Pereira de Oliveira <agavi2014@hotmail.com>
2019-09-25 17:10:28 -0300

    Mod Banco de dados

--- id10478708_controle.sql
+++ id10478708_controle.sql
@@ -1,25 +1,3 @@
-CREATE TABLE `historico` (
-  `id_manutencao` int(11) NOT NULL,
-  `id_patrimonio` int(11) NOT NULL,
-  `id_empresa` int(11) NOT NULL,
-  `id_empresa_manutencao` int(11) NOT NULL,
-  `nome_manutencao` varchar(50) DEFAULT NULL,
-  `descricao_problema` text DEFAULT NULL,
-  `data_manutencao` datetime DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-CREATE TABLE `nivel_user` (
-  `id_nivel` int(11) NOT NULL,
-  `nome_funcao` varchar(50) COLLATE utf8_bin DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
-CREATE TABLE `relatorio` (
-  `id_relatorio` int(11) NOT NULL,
-  `id_usuario` int(11) DEFAULT NULL,
-  `relatorio` varchar(500) COLLATE utf8_bin DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
-
 CREATE TABLE `user_array` (
   `id_user_array` int(11) NOT NULL,
   `id_usuario` int(11) NOT NULL,

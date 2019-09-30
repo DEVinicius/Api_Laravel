5dbf80d2deae4da5759d36e36d0ff66fce326ad4
Vinicius Pereira de Oliveira <agavi2014@hotmail.com>
2019-09-30 13:45:42 -0300

    Mudanças nas tabelas 2

--- app/Http/Controllers/Api/UserArrayController.php
+++ app/Http/Controllers/Api/UserArrayController.php
@@ -3,9 +3,21 @@
 namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\MasterApiController;
+use App\Models\UserArray;
 
-class UserArrayController extends Controller
+class UserArrayController extends MasterApiController
 {
-    //
+    protected $model;
+
+   protected $upload;
+
+   //Encontrar o caminho (pasta)
+   protected $path;
+
+   public function __construct(UserArray $pat, Request $req)
+   {   
+       $this -> model = $pat;
+       $this -> request = $req;
+   }
 }
--- app/User.php
+++ app/User.php
@@ -55,11 +55,6 @@ class User extends Authenticatable implements JWTSubject
         return $this->getKey();
     }
 
-    /**
-     * Return a key value array, containing any custom claims to be added to the JWT.
-     *
-     * @return array
-     */
     public function getJWTCustomClaims()
     {
         return [];
